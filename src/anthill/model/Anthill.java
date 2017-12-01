8ddfb95c666ded494264130f396172f0ac5ded46
yallay <yallay@Cyber-pout1n3>
2017-12-01 10:02:50 +0100

    Anthill Class

---
+++ src/anthill/model/Anthill.java
@@ -0,0 +1,29 @@
+package anthill.model;
+
+import java.util.List;
+
+public class Anthill {
+  List<Ant> listAnt;
+  int nbMaggot;
+  int nbChrysalis;
+  int nbEgg;
+  int nbWorker;
+  int nbPrince;
+  int nbPrincess;
+  
+  /**
+   * Anthill constructor.
+   */
+  public Anthill() {
+    nbMaggot = 0;
+    nbChrysalis = 0;
+    nbEgg = 0;
+    nbWorker = 0;
+    nbPrince = 0;
+    nbPrincess = 0;
+    listAnt = null;
+  }
+  
+  
+  
+}
