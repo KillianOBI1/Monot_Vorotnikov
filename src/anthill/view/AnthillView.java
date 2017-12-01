04afd8eee4d27c218c76be80cb5872eb4e0c1b03
Nikolai_Vorotnikov <rationalgathe@gmail.com>
2017-12-01 17:07:46 +0100

    view

---
+++ src/anthill/view/AnthillView.java
@@ -0,0 +1,82 @@
+package anthill.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.border.EmptyBorder;
+
+public class AnthillView {
+
+  private JFrame frame;
+
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          AnthillView window = new AnthillView();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  /**
+   * Create the application.
+   */
+  public AnthillView() {
+    initialize();
+  }
+
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 800, 476);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setLayout(new BorderLayout(0, 0));
+    
+    JMenuBar menuBar = new JMenuBar();
+    frame.getContentPane().add(menuBar, BorderLayout.NORTH);
+    
+    JMenu mnFile = new JMenu("File");
+    menuBar.add(mnFile);
+    
+    JMenuItem mntmExit = new JMenuItem("Exit");
+    mntmExit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        System.exit(JFrame.EXIT_ON_CLOSE);
+      }
+    });
+    mnFile.add(mntmExit);
+    
+    JMenu mnHelp = new JMenu("Help");
+    menuBar.add(mnHelp);
+    
+    JPanel statusBar = new JPanel();
+    statusBar.setBorder(new EmptyBorder(5, 5, 5, 5));
+    frame.getContentPane().add(statusBar, BorderLayout.SOUTH);
+    statusBar.setLayout(new BorderLayout(5, 5));
+    
+    JLabel lblStatus = new JLabel("Status :");
+    lblStatus.setHorizontalAlignment(SwingConstants.LEFT);
+    statusBar.add(lblStatus);
+  }
+
+}
