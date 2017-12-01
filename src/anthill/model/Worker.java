3827fb5ee6c678685a79081afa20a83808e3e97b
yallay <yallay@Cyber-pout1n3>
2017-12-01 11:10:42 +0100

    Worker class

---
+++ src/anthill/model/Worker.java
@@ -0,0 +1,30 @@
+package anthill.model;
+
+public class Worker extends Role {
+  Double carry;
+  
+  public Worker() {
+    super();
+  }
+  
+  void selfFeed(int weight) {
+    //this.foodQtty += weight;
+  }
+  
+  void feedOther(int id, int weight) {
+    
+  }
+  
+  void clean() {
+    
+  }
+  
+  void fight(Prey p) {
+    
+  }
+  
+  void feedMaggot(int id, int weight) {
+    
+  }
+  
+}
