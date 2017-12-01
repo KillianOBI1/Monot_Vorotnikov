23e47df7b5a85e8f7c61bd19060143e18e369307
yallay <yallay@Cyber-pout1n3>
2017-12-01 11:10:58 +0100

    Prey class

---
+++ src/anthill/model/Prey.java
@@ -0,0 +1,19 @@
+package anthill.model;
+
+import java.util.Date;
+
+public class Prey {
+  int weight;
+  Boolean isHunted;
+  Date dateDead;
+  //define position
+  
+  /**
+   * Prey constructor
+   */
+  public Prey() {
+    //weight =
+    isHunted = false;
+    dateDead = null;
+  }
+}
