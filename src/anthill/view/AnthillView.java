e2f1b338510275b9fad2979fbcacd11f5ee2807b
Nikolai_Vorotnikov <rationalgathe@gmail.com>
2017-12-04 09:03:51 +0100

    c1

--- src/anthill/view/AnthillView.java
+++ src/anthill/view/AnthillView.java
@@ -1,19 +1,17 @@
 package anthill.view;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.BoxLayout;
-import java.awt.BorderLayout;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import java.awt.FlowLayout;
 import javax.swing.border.EmptyBorder;
 
 public class AnthillView {
