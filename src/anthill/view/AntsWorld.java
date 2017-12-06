e6cf132dc925b903d2804baee4131126baa4b2eb
killianOBI1 <killian.monot@orange.fr>
2017-12-06 14:30:26 +0100

    Exception correct

--- src/anthill/view/AntsWorld.java
+++ src/anthill/view/AntsWorld.java
@@ -78,13 +78,21 @@ public class AntsWorld extends JPanel {
     for (Iterator<MovableDrawable> iter = ants.iterator(); iter.hasNext();) {
       iter.next().draw(g);
     }
-    
-    for (Iterator<MovableDrawable> iter = square.iterator(); iter.hasNext();) { 
-      Color test = this.getComponentAt(iter.next().getPosition()).getForeground();
+    List<MovableDrawable> contens = new LinkedList<>();
+    for (int i = 0; i < square.size(); i++) {
+      contens.add(square.get(i));
+    }
+    for (Iterator<MovableDrawable> iter = contens.iterator(); iter.hasNext();) { 
+      //Color test = this.getComponentAt(iter.next().getPosition()).getForeground();
       //if (!test.equals(Color.GREEN)) {
-      iter.next().draw(g);
-      
+      try {
+        iter.next().draw(g);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
       // }
     }
   }
+  
+  
 }
