58e5d125572c2f0f5b71be9e7e32d78fd247e759
Nikolai_Vorotnikov <rationalgathe@gmail.com>
2017-12-07 11:13:16 +0100

    test death corrected

--- src/anthill/tests/TestDeath.java
+++ src/anthill/tests/TestDeath.java
@@ -1,14 +1,11 @@
 package anthill.tests;
 
-import java.util.Random;
-
-import org.junit.jupiter.api.Test;
-
 import anthill.model.Ant;
 import anthill.model.Anthill;
 import anthill.model.roles.Queen;
 import anthill.model.states.Adult;
-import anthill.observer.Observer;
+
+import org.junit.jupiter.api.Test;
 
 class TestDeath {
 
@@ -17,19 +14,15 @@ class TestDeath {
     Ant queen = new Ant();
     queen.state = new Adult(new Queen());
     Anthill ah = queen.getState().getRole().ifQueen(queen).createAnthill(queen);
-    for (int i = 0 ; i < 100 ; i++) {
+    for (int i = 0 ; i < 10 ; i++) {
       Ant a = queen.getState().getRole().ifQueen(queen).createEgg(ah);
       ah.listAnt.add(a);
       ah.setEgg(1);
     }
     System.out.println(ah.listAnt.size());
-    Observer o = new Observer();
+
     for (Ant e :ah.listAnt) {
       System.out.println(e.getDateEnd() + " " + e.getAntId());
-    }
-    Random r = new Random();
-    while (true) {
-      System.out.println(r.nextInt(100));
     }
     
   }
