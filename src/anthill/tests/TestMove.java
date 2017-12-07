a3dafc53f308daa45c851c7231a960997ea89eb3
Nikolai_Vorotnikov <rationalgathe@gmail.com>
2017-12-07 11:46:36 +0100

    some chheckstyle in testMove

--- src/anthill/tests/TestMove.java
+++ src/anthill/tests/TestMove.java
@@ -1,16 +1,14 @@
 package anthill.tests;
 
-import java.util.Calendar;
-
-import org.junit.jupiter.api.Test;
-
 import anthill.model.Ant;
 import anthill.model.Anthill;
 import anthill.model.roles.Queen;
 import anthill.model.roles.Worker;
 import anthill.model.states.Adult;
-import anthill.observer.Observer;
 
+import java.awt.Point;
+import java.util.Calendar;
+import org.junit.jupiter.api.Test;
 
 class TestMove {
 
@@ -23,27 +21,23 @@ class TestMove {
     Ant a = queen.getState().getRole().ifQueen(queen).createEgg(ah);
     ah.listAnt.add(a);
     ah.setEgg(1);
-      
-    System.out.println(ah.listAnt.size());
         
     Calendar cal = Calendar.getInstance();
     cal.set(2017, 10, 05);
     System.out.println(cal.getTime());
     a.setDateStart(cal.getTime());
-    Observer o = new Observer();
+
     a.notifyToObserverEvol(ah);
     a.notifyToObserverEvol(ah);
     a.notifyToObserverEvol(ah);
-    for (Ant e :ah.listAnt) {
-      System.out.println(e.getDateEnd() + " " + e.getAntId() + " " + e.getState().getRole());
-    }
     
     Worker worker1 = (Worker) ah.listAnt.get(1).getState().getRole();
+    Point p = worker1.getPosition();
+    System.out.println(p);
+    
+    worker1.move();
     System.out.println(worker1.getPosition());
-    for (int i = 0; i < 100; i++) {
-      worker1.move();
-      System.out.println(worker1.getPosition());
-    }
+  
   }
 
 }
